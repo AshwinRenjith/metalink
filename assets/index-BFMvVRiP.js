a8268a5c8c86e4a12c591ccc6045d3580c413e82
Ashwin Renjith <royalty@Ashwins-MacBook-Air.local>
2025-04-12 22:18:08 +0530

    Updates

---
+++ assets/index-BFMvVRiP.js
@@ -0,0 +1,7 @@
+import{e as r,r as t}from"./index-BpRuMemm.js";/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=r("Info",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M12 16v-4",key:"1dtifu"}],["path",{d:"M12 8h.01",key:"e9boi3"}]]);var c=t.createContext(void 0);function i(e){const o=t.useContext(c);return e||o||"ltr"}export{a as I,i as u};
+//# sourceMappingURL=index-BFMvVRiP.js.map
--- src/types/express-validator.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'express-validator' {
-  import { Request } from 'express';
-
-  export function body(field: string, message?: string): any;
-  export function validationResult(req: Request): {
-    isEmpty(): boolean;
-    array(): Array<{ msg: string }>;
-  };
-} 
